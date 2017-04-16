6bb5adca20408191d9f91c431b7c047848a0db9b
dusekdan <dusekdan@gmail.com>
2017-04-16 07:06:10 +0200

    Finalized project functionality.

--- project.c
+++ project.c
@@ -1,143 +1,45 @@
-#include <stdlib.h> // Contains even thread safe functions like rand_r and drand48_x
+#include <stdlib.h>
 #include <stdio.h>
-
-// Threading includes
 #include <pthread.h>
-
-
-// intptr_t
 #include <stdint.h>
-
 #include <time.h>
 
+#define EXPECTED_NUMBER_OF_PARAMETERS 3
 #define RAND_EXCLUSIVE_RANGE 501
-#define DEBUG 1
+#define NANOSEC_MULTIPLIER 1000000
 #define DEBUG_YELLOW "\x1B[33m"
 #define DEBUG_DEFAULT "\x1B[0m"
-
 #define DECADIC_BASE 10
+#define DEBUG 0
 
-// TODO: INTRODUCE PROPER CONSTANTS
-
-// Critical section entry mutex initialization 
 int getticket (void);
 void await (int aenter);
 void advance (void);
 void debugMessage(char* message);
 void processParameters(char* argv[]);
 int getticket();
-void *ThreadWork(void *threadId);
+void *threadWork(void *threadId);
 unsigned int sleepThread(unsigned int *seed);
 void showHelp();
+unsigned int getRndThreadSleepTime(unsigned int *seed);
 
 struct Parameters
 {
     long threadCount;
     long passes;
 };
-
 struct Parameters P;
 
+pthread_mutex_t ticketDistributionMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t entryMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t wakeCond = PTHREAD_COND_INITIALIZER;
-
 int currentTicket = 1;
-
-
-
-void await (int aenter)
-{
-    // First thing to do is to prevent other threads from entering
-    if (pthread_mutex_lock(&entryMutex) != 0)
-    {
-        fprintf (stderr, "Error while using pthread_mutex_lock.\n");
-    }
-
-    while (currentTicket != aenter)
-    {
-        if (pthread_cond_wait(&wakeCond, &entryMutex) != 0)
-            fprintf(stderr, "Error while using pthread_cond_wait().\n");
-    }
-
-    //   Maybe any thread that calls this function will be freezed in case critical section is entered by other thread alreadye
-    //  printf ("Thread %d entered critical section.\n", aenter);
-}
-
-void advance ()
-{
-    if (pthread_mutex_unlock(&entryMutex) != 0)
-    {
-        fprintf (stderr, "Error while unlocking mutex.\n");
-    }
-    else
-    {
-        currentTicket++;
-        if (pthread_cond_broadcast(&wakeCond))
-        {
-            fprintf(stderr, "Waking broadcast error!\n");
-        }
-    }
-}
-
-
-
-
-
-void *ThreadWork(void *threadId)
-{
-    int ticket;
-
-    long tid;
-    tid = (intptr_t) threadId;
-
-    //printf ("Thread identifier #%ld was created and is now working!\n", tid);
-
-    // Prepare seed for random generator
-    unsigned int seed = (unsigned int) (time(NULL) + tid);
-
-
-    unsigned int debugSleepTimer1;
-    unsigned int debugSleepTimer2;
-
-
-    while ((ticket = getticket()) < P.passes)
-    {
-        // Initial sleep
-        debugSleepTimer1 = sleepThread(&seed);
-        //printf ("Thread #%ld was put to sleep for %ums\n", tid, debugSleepTimer1);
-
-        // await to enter critical section
-        await(ticket);
-        //printf ("Thread #%ld passed through await() function.\n", tid);
-
-
-        // Print who is in critical section
-        printf("%d\t(%d)\n", ticket, (int) tid);
-
-        // advance to leave critical section
-        advance();
-        //printf ("Tread #%ld passed through advance() function.\n", tid);
-
-        // Ending sleep
-        debugSleepTimer2 = sleepThread(&seed);
-        //printf ("Thread #%ld was put to sleep for %ums\n", tid, debugSleepTimer2);
-    }
-
-
-    pthread_exit(NULL);
-}
-
-
 int lastticket = 0;
-int getticket()
-{
-    lastticket += 1;
-    return lastticket;
-}
+
 
 int main (int argc, char* argv[])
 {
-    if (argc < 3)
+    if (argc < EXPECTED_NUMBER_OF_PARAMETERS)
     {
         showHelp();
         exit(1);
@@ -147,20 +49,21 @@ int main (int argc, char* argv[])
 
     if (DEBUG)
     {
-        debugMessage("Parameter parsing was successfull, program will now run.");   // God, I wish it was easier to work with integers, so I could print something more meaningful
+        debugMessage("Parameter parsing was successfull, program will now run.");
         printf ("%s", DEBUG_YELLOW);
         printf("Program will be run with N=%ld, M=%ld\n", P.threadCount, P.passes);
         printf ("%s", DEBUG_DEFAULT);
     }
 
-    // Spawn threads
+    // Spawn threads, prepare counters
+    // TODO: Make this more GNU like? 
     pthread_t threads[P.threadCount];
-    long threadCreationCode;
+    long threadCreationCode;    // FUTURE-TODO: Use this to detect error code 11 (EAGAIN), wait for a bit then try again 
     int i;
 
     for (i = 0; i < P.threadCount; i++)
     {
-        threadCreationCode = pthread_create (&threads[i], NULL, ThreadWork, (void *)(intptr_t)i);
+        threadCreationCode = pthread_create (&threads[i], NULL, threadWork, (void *)(intptr_t)i);
         if (threadCreationCode)
         {
             printf ("ERROR; return code from pthread_create() is %ld\n", threadCreationCode);
@@ -168,35 +71,145 @@ int main (int argc, char* argv[])
         }
     }
 
-    printf ("Some additional output from main thread...\n");
-
     // Clean up threads
     for (i = 0; i < P.threadCount; i++)
     {
         pthread_join (threads[i], NULL);
-        //printf ("Thread %i was collected.\n", i);
+        
+        if (DEBUG)
+            printf ("Thread %i was collected.\n", i);
     }
-
-    printf ("Some additional output from main thread...\n");
     
+    // Ensure the rest of the threads is not killed off when main thread reaches this point
     pthread_exit (NULL);
 
     return 0;
 }
 
 
-void debugMessage(char* message)
+/**
+  * threadWork()
+  * Start routine for pthread_create() that contains main skelet for the project
+  * void *threadId is a pthread API specific argument which in my case serves to
+  * pass a identification to thread.
+  */
+void *threadWork(void *threadId)
 {
-    printf("%s", DEBUG_YELLOW);
-    printf ("%s\n", message);
-    printf("%s", DEBUG_DEFAULT);
+    // Thread-local variables
+    int ticket;
+    long tid;
+    tid = (intptr_t) threadId;
+
+    // Prepare seed for random generator
+    unsigned int seed = (unsigned int) (time(NULL) + tid);
+
+    // Debug timing variables
+    unsigned int debugSleepTimer1;
+    unsigned int debugSleepTimer2;
+
+
+    while ((ticket = getticket()) <= P.passes)
+    {
+        // Initial sleep
+        debugSleepTimer1 = sleepThread(&seed);
+        if (DEBUG)
+            printf ("Thread #%ld was put to sleep for %ums\n", tid, debugSleepTimer1);
+
+        // Protect entry to critical section
+        await(ticket);
+        if (DEBUG)
+            printf ("Thread #%ld passed through await() function.\n", tid);
+
+
+        // Print who is in critical section
+        printf("%d\t(%d)\n", ticket, (int) tid);
+
+
+        // Leavy protected critical section and allow next one in
+        advance();
+        if (DEBUG)
+            printf ("Tread #%ld passed through advance() function.\n", tid);
+
+        // Termination sleep
+        debugSleepTimer2 = sleepThread(&seed);
+        if (DEBUG)
+            printf ("Thread #%ld was put to sleep for %ums\n", tid, debugSleepTimer2);
+    }
+
+    pthread_exit(NULL);
 }
+
+
+/**
+  * await()
+  * Protects critical section from multiple threads entering it at the same time
+  * int aenter - Integer ticket number identification of a thread that wants to enter
+  */
+void await (int aenter)
+{
+    // First thing to do is to prevent other threads from entering
+    if (pthread_mutex_lock(&entryMutex) != 0)
+    {
+        fprintf (stderr, "Error while using pthread_mutex_lock.\n");
+    }
+
+    // Using while + passive waiting to protect critical zone as suggested on:
+    // https://computing.llnl.gov/tutorials/pthreads/
+    while (currentTicket != aenter)
+    {
+        if (pthread_cond_wait(&wakeCond, &entryMutex) != 0)
+            fprintf(stderr, "Error while using pthread_cond_wait().\n");
+    }
+}
+
+
+/**
+  * advance()
+  * Unlocks critical section and sends wake up signal to all threads,
+  * threads whose ticket number is 'called' proceeds to await(), the
+  * rest is put to sleep again.
+  */
+void advance ()
+{
+    if (pthread_mutex_unlock (&entryMutex) != 0)
+    {
+        fprintf (stderr, "Error while unlocking mutex.\n");
+    }
+    else
+    {
+        // There is no need to lock mutex before incrementation, all other threads are asleep
+        currentTicket++;
+        if (pthread_cond_broadcast (&wakeCond))
+        {
+            fprintf(stderr, "Waking broadcast error!\n");
+        }
+    }
+}
+
+
+/**
+  * getticket()
+  * Gets a waiting ticket to critical section for thread, is mutex protected.
+  *
+  * NOTE: last ticket does not have to be declared static sinec it is considered
+  * shared among threads.
+  */
+int getticket ()
+{
+    // Mutex-Protected ticket number incrementation
+    pthread_mutex_lock (&ticketDistributionMutex);
+    lastticket += 1;
+    pthread_mutex_unlock (&ticketDistributionMutex);
+    
+    return lastticket;
+}
+
 
 /**
   * processParameters()
-  * Takes char* argv from main, converts its contents to integer and fills the Parameters P global variable
+  * Handles parameter processing for application
   */
-void processParameters(char* argv[])
+void processParameters (char* argv[])
 {
 
     char *endptr;
@@ -211,29 +224,64 @@ void processParameters(char* argv[])
         P.passes = strtol(argv[2], &endptr, DECADIC_BASE);
 }
 
-unsigned int getRndThreadSleepTime(unsigned int *seed)
+
+/**
+  * sleepThread()
+  * Sleeps thread for a random amount of time between 0.000s and 0.500s based on seed
+  * unsigned int* seed - time based seed for a thread
+  * returns number of miliseconds that thread was put to sleep
+  */
+unsigned int sleepThread (unsigned int *seed)
+{
+    // Get random sleep time from range
+    unsigned int sleepTime = getRndThreadSleepTime (seed);
+
+    // Compose timespec structure for nanosleep() function
+    struct timespec time;
+    time.tv_sec = 0;
+    time.tv_nsec = (sleepTime * NANOSEC_MULTIPLIER);
+
+    nanosleep (&time, NULL);
+
+    // Return amount of miliseconds that were slept (debug purposes, mainly)
+    return sleepTime;
+}
+
+
+/**
+  * getRndThreadSleepTime()
+  * Based on seed returns random number from within range 0-500
+  * unsigned int* seed
+  *
+  * NOTE: Since it is not the sole purpose of this project, I am satisfied with thread-safe
+  * pseudo-generator where randomness is not uniformly distributed. For the purpose of this
+  * project, this solution should be sufficient.
+  */
+unsigned int getRndThreadSleepTime (unsigned int *seed)
 {
     return (rand_r(seed) % RAND_EXCLUSIVE_RANGE);
 }
 
-unsigned int sleepThread(unsigned int *seed)
-{
 
-    unsigned int sleepTime = getRndThreadSleepTime(seed); 
-
-    struct timespec time;
-    time.tv_sec = 0;
-    time.tv_nsec = (sleepTime * 1000000);
-
-    nanosleep(&time, NULL);
-
-    return sleepTime;
-}
-
+/**
+  * Displays program's help screen
+  */
 void showHelp ()
 {
     printf ("Ticket algorithm synchronization demo.\n\n");
     printf ("Usage: ./XXX N M\n\n");
     printf ("Creates N threads and simulates M number of total passes through critical section protected by ticket algorithm.\n\n");
     printf ("No other available options. Wrong parameters to the program shows this help.\n");
+}
+
+
+/**
+  * Displays yellow debug message passed as parameter
+  * char* message - string message to be displayed in debug colors
+  */
+void debugMessage (char* message)
+{
+    printf ("%s", DEBUG_YELLOW);
+    printf ("%s\n", message);
+    printf ("%s", DEBUG_DEFAULT);
 }
